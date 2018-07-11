7b8ee9bda30e5818f6af322bd661c590fc3af006
haiyang1992 <haiyang1992@gmail.com>
2018-07-11 09:35:33 +0800

    first commit
    Signed-off-by: haiyang1992 <haiyang1992@gmail.com>

---
+++ code/chapter2/cgroup_demo.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+// root dir location of the memory subsystem hierarchy
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+// running top after running this code should show mem percentage = 500m/system memory size
+func main() {
+	if os.Args[0] == "/proc/self/exe" {
+		// container process
+		fmt.Printf("current pid %d", syscall.Getpid())
+		fmt.Println()
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 1000m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	cmd := exec.Command("/proc/self/exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	} else {
+		// get the pid (of the forked process) that was mapped to the outer namespace
+		fmt.Printf("%v", cmd.Process.Pid)
+
+		// create cgroup on the hierarchy which was automatically mounted a memory subsystem
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		// add container process to the cgroup
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+		// limit memory usage of cgroup
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("500m"), 0644)
+	}
+	cmd.Process.Wait()
+}
---
+++ code/chapter2/user_namespace.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+	}
+
+	// original code from the book doesn't work:
+	//cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
+	// instead try this:
+	// we can set the uid and gid of the new shell to any value we want, in this case 5001
+	cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{
+		{ContainerID: 5001, HostID: syscall.Getuid(), Size: 1},
+	}
+	cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{
+		{ContainerID: 5001, HostID: syscall.Getgid(), Size: 1},
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(-1)
+}
